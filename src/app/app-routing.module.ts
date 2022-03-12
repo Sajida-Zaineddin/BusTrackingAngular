2eba09d0b679e96d24dab45942935a0c01d8cd3c
Sajida Zaineddin <1837240@std.hu.edu.jo>
2022-03-12 06:09:52 +0300

    shared

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AuthModule } from './auth/auth.module';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutusComponent
+  },
+  {
+    path:'contact',
+    component:ContactusComponent
+  },
+  {
+    path:'testimonial',
+    component:TestimonialComponent
+  },
+  {
+    path:'account',
+    loadChildren:()=>AuthModule
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestimonialComponent,
     AboutusComponent,
-    ContactusComponent
+    ContactusComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
