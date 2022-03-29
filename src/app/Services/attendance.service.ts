6b3aa241fe2360f60b67f2a963456b3173274719
AseelAburumman <aseelaburumman10@gmail.com>
2022-03-29 11:19:56 +0300

    edit

--- src/app/Services/attendance.service.ts
+++ src/app/Services/attendance.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs'; 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,117 +15,122 @@ export class AttendanceService {
   busnumber:any=[];
   students:any=[];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private spinner :NgxSpinnerService,private toastr:ToastrService) { }
   getAll(){
-    //show spinner 
-   // this.spinner.show();
-    //hits Api 
+    
+   this.spinner.show();
+    
       this.http.get('https://localhost:44346/api/attendance/getall').subscribe((res)=>{
       this.attendance=res;
-     // this.spinner.hide();
-    //  this.toastr.success('Data Retrieved !!');
+     this.spinner.hide();
+    this.toastr.success('Data Retrieved !!');
     },err=>{
-     // this.spinner.hide();
-     // this.toastr.error(err.message, err.status);
+     this.spinner.hide();
+      this.toastr.error(err.message, err.status);
     })
   }
   search(name:any){
-    //  this.spinner.show();
-     // body.imagename=this.display_Image;
+    this.spinner.show();
       this.http.post('https://localhost:44346/api/student/SearchStudent/',name).subscribe((res)=>{
-
-      //  this.spinner.hide();
-       // this.toastr.success('saved Successfully :)');
+        this.name=res;
+      this.spinner.hide();
+       this.toastr.success('saved Successfully :)');
       },error=>{
-       // this.spinner.hide();
-       // this.toastr.error(error.status,error.message);
+       this.spinner.hide();
+       this.toastr.error(error.status,error.message);
       })
     }
  
   GETATTENDANCESTATUS(){
-    //show spinner 
-   // this.spinner.show();
-    //hits Api 
+     
+this.spinner.show();
+    
       this.http.get('https://localhost:44346/api/attendance/GetAttendance').subscribe((res)=>{
       this.status=res;
-     // this.spinner.hide();
-    //  this.toastr.success('Data Retrieved !!');
+  this.spinner.hide();
+ this.toastr.success('Data Retrieved !!');
     },err=>{
-     // this.spinner.hide();
-     // this.toastr.error(err.message, err.status);
+   this.spinner.hide();
+  this.toastr.error(err.message, err.status);
     })
   }
   
   GETSTUDENTNAME(){
-    //show spinner 
-   // this.spinner.show();
-    //hits Api 
+    
+   this.spinner.show();
+    
       this.http.get('https://localhost:44346/api/attendance/GetStudent').subscribe((res)=>{
       this.name=res;
-     // this.spinner.hide();
-    //  this.toastr.success('Data Retrieved !!');
+  this.spinner.hide();
+   this.toastr.success('Data Retrieved !!');
     },err=>{
-     // this.spinner.hide();
-     // this.toastr.error(err.message, err.status);
+    this.spinner.hide();
+    this.toastr.error(err.message, err.status);
     })
   }
   
   GETBUSNUMBER(){
-    //show spinner 
-   // this.spinner.show();
-    //hits Api 
+     
+   this.spinner.show();
+   
       this.http.get('https://localhost:44346/api/attendance/GetBusNum').subscribe((res)=>{
       this.busnumber=res;
-     // this.spinner.hide();
-    //  this.toastr.success('Data Retrieved !!');
+     this.spinner.hide();
+   this.toastr.success('Data Retrieved !!');
     },err=>{
-     // this.spinner.hide();
-     // this.toastr.error(err.message, err.status);
+   this.spinner.hide();
+      this.toastr.error(err.message, err.status);
     })
   }
   GetStudentList(){
-    //show spinner 
-   // this.spinner.show();
-    //hits Api 
+    
+this.spinner.show();
+     
       this.http.get('https://localhost:44346/api/bus/GetStudentList').subscribe((res)=>{
       this.students=res;
-     // this.spinner.hide();
-    //  this.toastr.success('Data Retrieved !!');
+    this.spinner.hide();
+     this.toastr.success('Data Retrieved !!');
     },err=>{
-     // this.spinner.hide();
-     // this.toastr.error(err.message, err.status);
+   this.spinner.hide();
+     this.toastr.error(err.message, err.status);
     })
   }
   
   
   create(attendance:any){
-    //  this.spinner.show();
-     // body.imagename=this.display_Image;
+    this.spinner.show();
+     
       this.http.post('https://localhost:44346/api/attendance/create/',attendance).subscribe((res)=>{
 
-      //  this.spinner.hide();
-       // this.toastr.success('saved Successfully :)');
+       this.spinner.hide();
+       this.toastr.success('saved Successfully :)');
       },error=>{
-       // this.spinner.hide();
-       // this.toastr.error(error.status,error.message);
+       this.spinner.hide();
+       this.toastr.error(error.status,error.message);
       })
     }
     update(attendance1:any){
-      //  body.imagename=this.display_Image;
      
         this.http.put('https://localhost:44346/api/attendance/update/',attendance1).subscribe((res)=>{
-        //  this.toastr.success('updated Successfully :)');
+       this.toastr.success('updated Successfully :)');
     
         },err=>{
-        //  this.toastr.error(err.status,err.message);
+        this.toastr.error(err.status,err.message);
         })
     
       } 
       delete(id:number){
         this.http.delete('https://localhost:44346/api/attendance/delete/'+id).subscribe((res)=>{
-         // this.toastr.success('Deleted Successfully :)');
+       this.toastr.success('Deleted Successfully :)');
         },err=>{
-        //  this.toastr.error(err.status,err.message);
+         this.toastr.error(err.status,err.message);
+        })
+      } 
+      sendEmail(){
+        this.http.delete('https://localhost:44346/api/attendance/SendEmail').subscribe((res)=>{
+       this.toastr.success('sent Successfully :)');
+        },err=>{
+         this.toastr.error(err.status,err.message);
         })
       }
 }
