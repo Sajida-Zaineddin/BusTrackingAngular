f6d7581f6bcb6507de2d27e7228e431b20ae13cc
eqbal97 <eqbal.saleh7@gmail.com>
2022-04-03 01:09:41 +0300

    autheraitaion

---
+++ src/app/parent-guard.guard.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParentGuardGuard implements CanActivate {
+  constructor(private router:Router,private toaster :ToastrService){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  
+      const token = localStorage.getItem('token');
+      if (token) {
+        console.log(state);
+      
+        if (state.url.indexOf('parent') >= 0) {
+          let user: any = localStorage.getItem('user');
+          if (user) {
+            debugger
+            user = JSON.parse(user);
+            if (user.role == 'Parent') {
+              this.toaster.success('welcome');
+              return true;
+            }
+       
+            else {
+              this.toaster.warning('this page for Parent');
+              return false;
+            }
+            
+          }
+  
+          else {
+            this.toaster.warning('role name is undfined ');
+            return false;
+          }
+        }
+  
+        return true;
+      }
+  
+      else {
+        this.router.navigate(['auth/login']);
+        this.toaster.warning('Please login !!')
+        return false;
+      }
+  
+  }
+  
+}
---
+++ src/app/teacher-gurad.guard.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherGuradGuard implements CanActivate {
+ 
+      constructor(private router:Router,private toaster :ToastrService){}
+      canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+         
+          const token = localStorage.getItem('token');
+          if (token) {
+            console.log(state);
+          
+            if (state.url.indexOf('teacher') >= 0) {
+              let user: any = localStorage.getItem('user');
+              if (user) {
+               
+                user = JSON.parse(user);
+                if (user.role == 'Teacher') {
+                  this.toaster.success('welcome');
+                  return true;
+                }
+           
+                else {
+                  this.toaster.warning('this page for Teacher');
+                  this.router.navigate(['security/login']);
+                  return false;
+                }
+                
+              }
+      
+              else {
+                this.toaster.warning('role name is undfined ');
+                return false;
+              }
+            }
+      
+            return true;
+          }
+      
+          else {
+            this.router.navigate(['auth/login']);
+            this.toaster.warning('Please login !!')
+            return false;
+          }
+      
+        }
+  }
+  
+
