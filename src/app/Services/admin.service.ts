bf0250be8f30d00d7d661c9f136178076846247b
eqbal97 <eqbal.saleh7@gmail.com>
2022-04-02 18:28:39 +0300

    map v6

--- src/app/Services/admin.service.ts
+++ src/app/Services/admin.service.ts
@@ -12,6 +12,12 @@ export class AdminService {
   constructor(private http: HttpClient, private spinner: NgxSpinnerService, private toastr: ToastrService) { }
 
 
+  
+  allStudents:any=[];
+  allTeachers:any=[];
+  allDrivers:any=[];
+ 
+
 
   getAllBusesRoute() {   
     this.http.get('https://localhost:44346/api/route/GetAll').subscribe((res) => {
@@ -21,5 +27,32 @@ export class AdminService {
     })
   }
 
+  getAllStudentsData() {   
+    this.http.get('https://localhost:44346/api/student/GetAll/').subscribe((res) => {
+      this.allStudents=res
+  
+    }, err => {    
+      this.toastr.error('Error ')
+    })
+    
+  }
+  getAllTeachersData() {   
+    this.http.get('https://localhost:44346/api/user/GetAllTeachers/').subscribe((res) => {
+      this.allTeachers=res
+  
+    }, err => {    
+      this.toastr.error('Error ')
+    })
+    
+  }
+  getAllDriversData() {   
+    this.http.get('https://localhost:44346/api/user/GetAllDrivers/').subscribe((res) => {
+      this.allDrivers=res
+  
+    }, err => {    
+      this.toastr.error('Error ')
+    })
+    
+  }
 
 }
