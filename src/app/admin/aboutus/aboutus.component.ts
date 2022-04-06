79ef78fe4753d4d9201521613c6a01421b11f178
Sajida Zaineddin <1837240@std.hu.edu.jo>
2022-04-06 05:05:46 +0300

    updateAboutus

--- src/app/admin/aboutus/aboutus.component.ts
+++ src/app/admin/aboutus/aboutus.component.ts
@@ -18,13 +18,9 @@ export class AboutusComponent implements OnInit {
   @ViewChild('callDeleteDialog') callDeleteDialog!: TemplateRef<any>
   @ViewChild('callCreateDialog') callCreateDialog!: TemplateRef<any>
 
-
-
   about: any = {}
 
   constructor(private dialog: MatDialog, public home: AboutusService) { }
-
-
 
   updatForm: FormGroup = new FormGroup({
     id: new FormControl(),
@@ -33,18 +29,15 @@ export class AboutusComponent implements OnInit {
     imagepath: new FormControl()   
   })
 
-
   CreateForm :FormGroup =new FormGroup({
-  
     title:new FormControl('',Validators.required),
     information:new FormControl('',Validators.required),
-    
     imagepath:new FormControl()
   })
+
   ngOnInit(): void {
     this.home.getAll();
   }
-
 
   save(){
     this.home.createAboutus(this.CreateForm.value);
@@ -53,7 +46,6 @@ export class AboutusComponent implements OnInit {
 
   openCreatedialog() {
     this.dialog.open(this.callCreateDialog)
-
   }
 
   openDeleteDialog(Aboutusid: any) {
@@ -66,14 +58,12 @@ export class AboutusComponent implements OnInit {
         }
         else if (res == "no")
           console.log("Thank you ");
-
       }
     })
-
   }
+
   openUpdateDialog(id: any, title: any, info: any, imagename2: any) {
     console.log("openUpdateDialog",id,title,info, imagename2);
-
     this.about = {
       id: id,
       title: title,
@@ -83,37 +73,26 @@ export class AboutusComponent implements OnInit {
     console.log("about.imagepath",this.about.imagepath);
     this.updatForm.controls['id'].setValue(id);
     this.updatForm.controls['imagepath'].setValue(imagename2);
-
     this.dialog.open(this.callUpdateDialog)
-
   }
 
   uploadFile(file:any){
     if(file.length===0){
       return ;
-
     }
     let fileUpload=<File>file[0];
     // file[0]:'angular.png';
     const fromData=new FormData();
     fromData.append('file',fileUpload,fileUpload.name);
     this.home.uploadAttachment(fromData);
-    
   }
 
-  updateCourse(){
+  update(){
     console.log("updatForm",this.updatForm.value);
     console.log("about",this.about);
-
     this.home.updateAboutus(this.updatForm.value);
     window.location.reload();
-
   }
-
-
-
-
-
 }
 
 
